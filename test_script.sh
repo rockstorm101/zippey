d3b97da8a402840e8d90a6eef4a40404dd54b9e3
Kristian Høy Horsberg <khh1990@gmail.com>
2014-05-18 04:01:21 +0200

    Fixed test script

--- test_script.sh
+++ test_script.sh
@@ -1,17 +1,17 @@
 #/bin/sh
 
-python zip_filter.py e <winword.docx >winword.plain
-python zip_filter.py d <winword.plain >winword_new.docx
+python zippey.py e < winword.docx > winword.plain
+python zippey.py d < winword.plain > winword_new.docx
 
 
-mkdir tmp/winword
-mkdir tmp/winword_new
-
-rm -rf winword
-rm -rf winword_new
+mkdir -p tmp/winword
+mkdir -p tmp/winword_new
 
 unzip -q winword.docx -d tmp/winword
 unzip -q winword_new.docx -d tmp/winword_new
+
+rm -rf winword.plain
+rm -rf winword_new.docx
 
 diff -rq tmp/winword tmp/winword_new
 
